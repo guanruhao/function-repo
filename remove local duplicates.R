f396ce6f173d39286b8c841bba452c0ac466e5cd
guanruhao <98945451+guanruhao@users.noreply.github.com>
2023-03-13 10:00:23 +0800

    Add files via upload

---
+++ remove local duplicates.R
@@ -0,0 +1,28 @@
+
+remove_local_duplicates=function(vector1, vector2) {
+  # build empty lists
+  res1=c()
+  res2=c()
+  # see vector1
+  for (i in 1:(length(vector1)-1)) {
+    #if the next element is not the same as current,then put them in to results
+    if (vector1[i] != vector1[i+1]) {
+      res1=c(res1, vector1[i])
+      res2=c(res2, vector2[i])
+    }
+  }
+  # put the last element in to results
+  res1=c(res1, vector1[length(vector1)])
+  res2=c(res2, vector2[length(vector2)])
+  # return results
+  return(list(res1, res2))
+}
+
+
+
+
+
+
+
+
+
